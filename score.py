0c0b4e833a21ccd8ef21526a1b4f86b137796007
Robbie Lambe <robbielambe@gmail.com>
2017-02-05 10:44:08 +1100

    Adding score script
    Score script

---
+++ score.py
@@ -0,0 +1,32 @@
+#
+# Script for checking the score of a grade
+# Robert Lambe
+#
+
+# Get input
+inp = raw_input("Enter Score: ")
+score = float(inp)
+
+# Calculate score
+if score < 0 :
+    print "Errorrr"
+    quit()
+    
+elif score > 1 :
+    print "Eeeerror"
+    quit()
+    
+elif score >= 0.9 :
+    print "A"
+    
+elif score >= 0.8 :
+    print "B"
+
+elif score >= 0.7 :
+    print "C"
+    
+elif score >= 0.6 :
+    print "D"
+
+elif score < 0.6 :
+    print "F"
