c1d37e276769ad34b8621512e3c5bb5d6e85203f
Robbie Lambe <robbielambe@gmail.com>
2017-02-05 11:48:05 +1100

    Hours 1 and 2
    Added scripts for hours1 and hours2

---
+++ hours1.py
@@ -0,0 +1,18 @@
+
+try :
+	inp = raw_input("Enter Hours: ")
+	hours = float(inp)
+	inp = raw_input("Enter Rate: ")
+	rate = float(inp)
+
+except:
+	print "ah here, thats not a float.. "
+	quit()
+
+	print rate, hours
+	if hours <= 40 : 
+		pay = rate * hours
+	else :
+		pay = rate * 40 + ( rate * 1.5 * ( hours - 40) )
+	print pay
+
